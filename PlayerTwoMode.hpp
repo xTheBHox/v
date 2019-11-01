b4ffb9fc5226cd7af92ca835e51e7121c70c52eb
xTheBHox <35213314+xTheBHox@users.noreply.github.com>
2019-11-01 14:10:39 -0400

    Made P2 inherit P1

--- PlayerTwoMode.hpp
+++ PlayerTwoMode.hpp
@@ -5,11 +5,11 @@
 #include "Mesh.hpp"
 #include "GameLevel.hpp"
 #include "Connection.hpp"
-#include "GameLevel.hpp"
+#include "PlayerOneMode.hpp"
 
 #include <list>
 
-struct PlayerTwoMode : Mode {
+struct PlayerTwoMode : PlayerOneMode {
   PlayerTwoMode(GameLevel *level_, std::string const &host, std::string const &port);
 	virtual ~PlayerTwoMode();
 
@@ -17,36 +17,10 @@ struct PlayerTwoMode : Mode {
 	virtual void update(float elapsed) override;
   virtual void draw(glm::uvec2 const &drawable_size) override;
 
-  //Current control signals:
-	struct {
-		bool forward = false;
-		bool backward = false;
-		bool left = false;
-		bool right = false;
-    bool sprint = false;
-    bool jump = false;
-    bool mouse_down = false;
-    float mouse_sensitivity = 4.0f;
-	} controls;
-
   struct {
     bool flat = false;
     float drag_sensitivity = 20.0f;
   } controls_shift;
-
-	bool DEBUG_fly = false;  //fly around for collsion debug
-  bool pause = false;
-
-  // Player camera tracked using this structure:
-  struct {
-    Scene::Camera *camera = nullptr;
-    Scene::Transform *body = nullptr;
-    glm::vec3 vel = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 gravity = glm::vec3(0.0f, 0.0f, -100.0f);
-    float azimuth = 0.0f;
-    float elevation = 30.0f / 180.0f * 3.1415926f;
-    bool in_air = false;
-  } pov;
 
   struct {
     GameLevel::Movable *moving = nullptr;
@@ -54,8 +28,6 @@ struct PlayerTwoMode : Mode {
     float progress = 0.0f;
     float speed = 4.0f; // 1/t, t is the time taken to shift
   } shift;
-
-  GameLevel *level;
 
   std::unique_ptr< Client > client = nullptr;
 
